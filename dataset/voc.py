correct loss calculation, correct train id list

dataset/voc.py
@@ -49,24 +49,18 @@ class VOCDataset(Dataset):
         if split == 'trainval':
             self.split = 'trainval'
             # data_path = os.path.join(data_path, 'VOC2007-trainval')
-            # data_path = os.path.join(data_path, 'VOC2007-trainval-petit')
             data_path = os.path.join(data_path, 'VOC2007')
         elif split == 'test':
             self.split = 'test'
             data_path = os.path.join(data_path, 'VOC2007-test')
-        # self.data_path = os.path.join(data_path, 'VOC2007')
+        # data_path = os.path.join(data_path, 'VOC2007')
         self.data_path = data_path
         self.annopath = os.path.join(self.data_path, 'Annotations', '%s.xml')
         self.imgpath = os.path.join(self.data_path, 'JPEGImages', '%s.jpg')
-        self.imgsetpath = os.path.join(self.data_path, 'ImageSets', 'Layout', '%s.txt')
-        # self.imgsetpath = os.path.join(self.data_path, 'ImageSets', 'Main', 'dog_%s.txt')
+        self.imgsetpath = os.path.join(self.data_path, 'ImageSets', 'Main', '%s.txt')
 
         # get image ids
         self.ids = list()
-        # for line in open(self.imgsetpath % self.split):
-        #     part1, part2 = line.strip().split()
-        #     if part2 == '1':
-        #         self.ids.append(part1)
         for line in open(self.imgsetpath % self.split):
             self.ids.append(line.strip())
 
@@ -136,5 +130,5 @@ class VOCDataset(Dataset):
                 x1 = im_w - x1 - w
                 x2 = x1 + w
                 targets['bboxes'][idx] = torch.as_tensor([x1, y1, x2, y2])
-                
+
         return img_id, img, targets

infer.py
@@ -1,6 +1,6 @@
 import torch
 import numpy as np
-# import cv2
+import cv2
 import argparse
 import random
 import os
@@ -173,91 +173,99 @@ def load_model_and_dataset():
     faster_rcnn_model = FasterRCNN(device)
     faster_rcnn_model.eval()
     faster_rcnn_model.to(device)
-    faster_rcnn_model.load_state_dict(torch.load('/home/infres/ryang-23/fasterRCNN_implementation/result/model.pth',
+    # faster_rcnn_model.load_state_dict(torch.load('/home/infres/ryang-23/fasterRCNN_implementation/result/model.pth',
+    faster_rcnn_model.load_state_dict(torch.load('result/model.pth',
                                                  map_location=device))
     return faster_rcnn_model, voc, test_dataset
 
 
-# def infer(args):
-#     if not os.path.exists('samples'):
-#         os.mkdir('samples')
-#     faster_rcnn_model, voc, test_dataset = load_model_and_dataset(args)
+def infer():
+    if not os.path.exists('samples'):
+        os.mkdir('samples')
+    faster_rcnn_model, voc, test_dataset = load_model_and_dataset()
     
-#     # Hard coding the low score threshold for inference on images for now
-#     # Should come from config
-#     faster_rcnn_model.roi_head.threshold = 0.7
+    # Hard coding the low score threshold for inference on images for now
+    # Should come from config
+    faster_rcnn_model.roi_head.threshold = 0.7
     
-#     for sample_count in tqdm(range(10)):
-#         random_idx = random.randint(0, len(voc))
-#         im, target, fname = voc[random_idx]
-#         im = im.unsqueeze(0).float().to(device)
+    for sample_count in tqdm(range(30)):
+        random_idx = random.randint(0, len(voc))
+        img_id, im, target = voc[random_idx]
+        print(img_id)
+        im = im.unsqueeze(0).float().to(device)
 
-#         # gt_im = cv2.imread(fname)
-#         # gt_im_copy = gt_im.copy()
-        
-#         # Saving images with ground truth boxes
-#         for idx, box in enumerate(target['bboxes']):
-#             x1, y1, x2, y2 = box.detach().cpu().numpy()
-#             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+        fname = voc.imgpath % str(img_id)
+        # print(fname)
+        gt_im = cv2.imread(fname)
+        gt_im_copy = gt_im.copy()
+
+
+        # Saving images with ground truth boxes
+        for idx, box in enumerate(target['bboxes']):
+            x1, y1, x2, y2 = box.detach().cpu().numpy()
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             
-        #     cv2.rectangle(gt_im, (x1, y1), (x2, y2), thickness=2, color=[0, 255, 0])
-        #     cv2.rectangle(gt_im_copy, (x1, y1), (x2, y2), thickness=2, color=[0, 255, 0])
-        #     text = voc.idx2label[target['labels'][idx].detach().cpu().item()]
-        #     text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
-        #     text_w, text_h = text_size
-        #     cv2.rectangle(gt_im_copy , (x1, y1), (x1 + 10+text_w, y1 + 10+text_h), [255, 255, 255], -1)
-        #     cv2.putText(gt_im, text=voc.idx2label[target['labels'][idx].detach().cpu().item()],
-        #                 org=(x1+5, y1+15),
-        #                 thickness=1,
-        #                 fontScale=1,
-        #                 color=[0, 0, 0],
-        #                 fontFace=cv2.FONT_HERSHEY_PLAIN)
-        #     cv2.putText(gt_im_copy, text=text,
-        #                 org=(x1 + 5, y1 + 15),
-        #                 thickness=1,
-        #                 fontScale=1,
-        #                 color=[0, 0, 0],
-        #                 fontFace=cv2.FONT_HERSHEY_PLAIN)
-        # cv2.addWeighted(gt_im_copy, 0.7, gt_im, 0.3, 0, gt_im)
-        # cv2.imwrite('samples/output_frcnn_gt_{}.png'.format(sample_count), gt_im)
+            cv2.rectangle(gt_im, (x1, y1), (x2, y2), thickness=2, color=[0, 255, 0])
+            cv2.rectangle(gt_im_copy, (x1, y1), (x2, y2), thickness=2, color=[0, 255, 0])
+            text = voc.idx2label[target['labels'][idx].detach().cpu().item()]
+            text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
+            text_w, text_h = text_size
+            cv2.rectangle(gt_im_copy , (x1, y1), (x1 + 10+text_w, y1 + 10+text_h), [255, 255, 255], -1)
+            cv2.putText(gt_im, text=voc.idx2label[target['labels'][idx].detach().cpu().item()],
+                        org=(x1+5, y1+15),
+                        thickness=1,
+                        fontScale=1,
+                        color=[0, 0, 0],
+                        fontFace=cv2.FONT_HERSHEY_PLAIN)
+            cv2.putText(gt_im_copy, text=text,
+                        org=(x1 + 5, y1 + 15),
+                        thickness=1,
+                        fontScale=1,
+                        color=[0, 0, 0],
+                        fontFace=cv2.FONT_HERSHEY_PLAIN)
+        cv2.addWeighted(gt_im_copy, 0.7, gt_im, 0.3, 0, gt_im)
+        cv2.imwrite('samples/output_frcnn_gt_{}.png'.format(sample_count), gt_im)
         
         # Getting predictions from trained model
-        # rpn_output, frcnn_output = faster_rcnn_model(im, None)
-        # boxes = frcnn_output['boxes']
-        # labels = frcnn_output['labels']
-        # scores = frcnn_output['scores']
-        # im = cv2.imread(fname)
-        # im_copy = im.copy()
+        rpn_output, frcnn_output = faster_rcnn_model(im, None)
+        boxes = frcnn_output['bboxes']
+        labels = frcnn_output['labels']
+        scores = frcnn_output['scores']
+        im = cv2.imread(fname)
+        im_copy = im.copy()
         
-        # # Saving images with predicted boxes
-        # for idx, box in enumerate(boxes):
-        #     x1, y1, x2, y2 = box.detach().cpu().numpy()
-        #     x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-        #     cv2.rectangle(im, (x1, y1), (x2, y2), thickness=2, color=[0, 0, 255])
-        #     cv2.rectangle(im_copy, (x1, y1), (x2, y2), thickness=2, color=[0, 0, 255])
-        #     text = '{} : {:.2f}'.format(voc.idx2label[labels[idx].detach().cpu().item()],
-        #                                 scores[idx].detach().cpu().item())
-        #     text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
-        #     text_w, text_h = text_size
-        #     cv2.rectangle(im_copy , (x1, y1), (x1 + 10+text_w, y1 + 10+text_h), [255, 255, 255], -1)
-        #     cv2.putText(im, text=text,
-        #                 org=(x1+5, y1+15),
-        #                 thickness=1,
-        #                 fontScale=1,
-        #                 color=[0, 0, 0],
-        #                 fontFace=cv2.FONT_HERSHEY_PLAIN)
-        #     cv2.putText(im_copy, text=text,
-        #                 org=(x1 + 5, y1 + 15),
-        #                 thickness=1,
-        #                 fontScale=1,
-        #                 color=[0, 0, 0],
-        #                 fontFace=cv2.FONT_HERSHEY_PLAIN)
-        # cv2.addWeighted(im_copy, 0.7, im, 0.3, 0, im)
-        # cv2.imwrite('samples/output_frcnn_{}.jpg'.format(sample_count), im)
+        # Saving images with predicted boxes
+        for idx, box in enumerate(boxes):
+            x1, y1, x2, y2 = box.detach().cpu().numpy()
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            cv2.rectangle(im, (x1, y1), (x2, y2), thickness=2, color=[0, 0, 255])
+            cv2.rectangle(im_copy, (x1, y1), (x2, y2), thickness=2, color=[0, 0, 255])
+            text = '{} : {:.2f}'.format(voc.idx2label[labels[idx].detach().cpu().item()],
+                                        scores[idx].detach().cpu().item())
+            text_size, _ = cv2.getTextSize(text, cv2.FONT_HERSHEY_PLAIN, 1, 1)
+            text_w, text_h = text_size
+            cv2.rectangle(im_copy , (x1, y1), (x1 + 10+text_w, y1 + 10+text_h), [255, 255, 255], -1)
+            cv2.putText(im, text=text,
+                        org=(x1+5, y1+15),
+                        thickness=1,
+                        fontScale=1,
+                        color=[0, 0, 0],
+                        fontFace=cv2.FONT_HERSHEY_PLAIN)
+            cv2.putText(im_copy, text=text,
+                        org=(x1 + 5, y1 + 15),
+                        thickness=1,
+                        fontScale=1,
+                        color=[0, 0, 0],
+                        fontFace=cv2.FONT_HERSHEY_PLAIN)
+        cv2.addWeighted(im_copy, 0.7, im, 0.3, 0, im)
+        cv2.imwrite('samples/output_frcnn_{}.jpg'.format(sample_count), im)
 
 
 def evaluate_map():
     faster_rcnn_model, voc, test_dataset = load_model_and_dataset()
+
+    faster_rcnn_model.roi_head.threshold = 0.7
+
     gts = []
     preds = []
     for one_batch in tqdm(test_dataset):
@@ -318,4 +326,5 @@ if __name__ == '__main__':
     #     evaluate_map(args)
     # else:
     #     print('Not Evaluating as `evaluate` argument is False')
-    evaluate_map()
+    # evaluate_map()
+    infer()

model/fasterRCNN.py
@@ -313,8 +313,8 @@ class RPN(torch.nn.Module):
 
             localization_loss = (
                 torch.nn.functional.smooth_l1_loss(
-                    reg_pred[sampled_idx],
-                    regression_targets[sampled_idx],
+                    reg_pred[sampled_pos_idx_mask],
+                    regression_targets[sampled_pos_idx_mask],
                     beta = 1/9,
                     reduction='sum',
                 ) / (sampled_idx.numel())

train.py
@@ -14,8 +14,8 @@ from torch.optim.lr_scheduler import MultiStepLR
 
 def train():
     # Load the YAML configuration
-    with open('config/voc.yaml', 'r') as file:
-        config = yaml.safe_load(file)
+    # with open('config/voc.yaml', 'r') as file:
+    #     config = yaml.safe_load(file)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = FasterRCNN(device).to(device)
@@ -48,8 +48,8 @@ def train():
     train_dataset = voc.VOCDataset(split='trainval')
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
         
-    num_epochs = config['train_param']['num_epochs']
-    num_epochs = 300
+    # num_epochs = config['train_param']['num_epochs']
+    num_epochs = 100
     for epoch in range(num_epochs):
         model.train()
         train_bar = tqdm.tqdm(train_loader, file = sys.stdout, ncols=100)
@@ -112,7 +112,7 @@ def train():
     print("LOSSES: ", avg_losses)
     print("LOSSES_RPNS: ", avg_losses_rpns)
     print("LOSSES_FRCNNS: ", avg_losses_frcnns)
-    
+
 
     # save the model
     torch.save(model.state_dict(), os.path.join(save_path, 'model.pth'))