ec7b1e9b1be82da09896158da959f1a632014c42
Ruoxuan Yang <ruoxuan.yang@telecom-paris.fr>
2024-10-31 20:25:15 +0100

    change epoch number to 20:

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def train():
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
         
     # num_epochs = config['train_param']['num_epochs']
-    num_epochs = 30
+    num_epochs = 20
     for epoch in range(num_epochs):
         model.train()
         train_bar = tqdm.tqdm(train_loader, file = sys.stdout, ncols=100)
