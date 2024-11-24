e3c41b9acdc3d275d9df60d095fb332cbd38c366
Ruoxuan Yang <ruoxuan.yang@telecom-paris.fr>
2024-11-24 14:34:29 +0100

    use caffe backbone

--- model/fasterRCNN.py
+++ model/fasterRCNN.py
@@ -671,6 +671,7 @@ class FasterRCNN(torch.nn.Module):
         img = img * 255
         # img = self.transform(img)
         # img = img[[2, 1, 0], ...]
+        img = img[:, [2, 1, 0], :, :]
         # Subtract the Caffe mean (values in [0, 255] range)
         # mean = torch.tensor([103.939, 116.779, 123.68]).view(3, 1, 1).to(self.device)
         # img = img - mean
